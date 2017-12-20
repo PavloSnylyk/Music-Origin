cc34c1c259a334b089a650d926449b7ae99cbaea
Pavlo Snylyk <stanova94@gmail.com>
2017-12-20 16:45:33 +0200

    first commit

---
+++ MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addMusicButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nextSongButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void previousSongButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void randomButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
