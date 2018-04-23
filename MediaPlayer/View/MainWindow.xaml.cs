e209130e2d7fd09608bf79360a4f1b4d2611c21e
Bohdan Zaiats <bohdanzaiats999@.gmail.com>
2018-04-23 22:26:40 +0300

    Added localization

--- MediaPlayer/View/MainWindow.xaml.cs
+++ MediaPlayer/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using MusicOrigin.ViewModel;
 using MusicOrigin.Services;
+using System.Globalization;
 
 namespace MusicOrigin.View
 {
@@ -23,9 +24,49 @@ namespace MusicOrigin.View
         public MainWindow()
         {
             InitializeComponent();
+
+            App.LanguageChanged += LanguageChanged;
+
+            CultureInfo currLang = App.Language;
+
+            menuLanguage.Items.Clear();
+            foreach (var lang in App.Languages)
+            {
+                MenuItem menuLang = new MenuItem();
+                menuLang.Header = lang.DisplayName;
+                menuLang.Tag = lang;
+                menuLang.IsChecked = lang.Equals(currLang);
+                menuLang.Click += ChangeLanguageClick;
+                menuLanguage.Items.Add(menuLang);
+            }
             MusicOriginViewModel viewModel = new MusicOriginViewModel(new DialogService(), new SongFileService());
             DataContext = viewModel;
             Closing += viewModel.OnWindowClosing;
         }
+
+        private void LanguageChanged(Object sender, EventArgs e)
+        {
+            CultureInfo currLang = App.Language;
+
+            foreach (MenuItem i in menuLanguage.Items)
+            {
+                CultureInfo ci = i.Tag as CultureInfo;
+                i.IsChecked = ci != null && ci.Equals(currLang);
+            }
+        }
+
+        private void ChangeLanguageClick(Object sender, EventArgs e)
+        {
+            MenuItem mi = sender as MenuItem;
+            if (mi != null)
+            {
+                CultureInfo lang = mi.Tag as CultureInfo;
+                if (lang != null)
+                {
+                    App.Language = lang;
+                }
+            }
+        }
     }
 }
+
